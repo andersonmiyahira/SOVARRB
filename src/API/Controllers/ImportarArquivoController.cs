87f28adf608b5d908055283aaef28ec3cfe59fa9
Anderson <andersonmiyahira@gmail.com>
2018-04-29 17:34:12 -0300

    - adicionando controller de upload e cors

---
+++ src/API/Controllers/ImportarArquivoController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/ImportarArquivo")]
+    public class ImportarArquivoController : Controller
+    {
+        [HttpPost]
+        [Route("File")]
+        public IActionResult PostFile(IFormFile file)
+        {
+            return Ok();
+
+            //if (file == null || file.Length <= 0)
+            //{
+                //NotifyError("400", "Arquivo inválido.");
+              
+            //}
+
+            //var retorno = _uploadService.UploadFile(file);
+            //if (!retorno.IsValid)
+            //{
+            //    NotifyError("400", retorno.Erro);
+            //    return Response();
+            //}
+
+            //return Response(retorno);
+        }
+    }
+}

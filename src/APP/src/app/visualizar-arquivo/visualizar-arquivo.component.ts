b332f4c80535689409ac4fe2f1f7691a2c87ea76
Rafael Farias <evertonrafael00@gmail.com>
2018-06-06 22:37:18 -0300

    ajus de layout - visualizar-arquivo

--- src/APP/src/app/visualizar-arquivo/visualizar-arquivo.component.ts
+++ src/APP/src/app/visualizar-arquivo/visualizar-arquivo.component.ts
@@ -7,6 +7,7 @@ import { NgbModule, NgbDatepickerModule, NgbModal, ModalDismissReasons } from '@
   templateUrl: './visualizar-arquivo.component.html',
   styleUrls: ['./visualizar-arquivo.component.css']
 })
+
 export class VisualizarArquivoComponent implements OnInit {
   logoDatePickerUrl = "../../assets/img/calendar-icon.svg";
   errors: any;

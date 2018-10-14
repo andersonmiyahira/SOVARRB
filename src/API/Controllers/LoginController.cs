b747cf1b6c9370ad33ceb87af81ec3c92eb09b11
Anderson <andersonmiyahira@gmail.com>
2018-10-14 17:53:47 -0300

    adicionando jwt token de autenticacao

---
+++ src/API/Controllers/LoginController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Principal;
+using Microsoft.IdentityModel.Tokens;
+using API.ControllerBaseExtensions;
+using Application.AppService.ValorEsperado;
+using Application.ViewModel.Request;
+using API.Models;
+
+namespace API.Controllers
+{
+    [Produces("application/json")]
+    public class LoginController : ApiController
+    {
+        private readonly IUsuarioAppService _usuarioAppService;
+
+        public LoginController(IUsuarioAppService usuarioAppService)
+        {
+            _usuarioAppService = usuarioAppService;
+        }  
+
+        [AllowAnonymous]
+        [HttpPost]
+        public object Post(
+            [FromBody]UsuarioRequest usuario,
+            [FromServices]SigningConfigurations signingConfigurations,
+            [FromServices]TokenConfigurations tokenConfigurations)
+        {
+            bool credenciaisValidas = true;
+            //if (usuario != null && !String.IsNullOrWhiteSpace(usuario.UserID))
+            //{
+            //    var usuarioBase = usersDAO.Find(usuario.UserID);
+            //    credenciaisValidas = (usuarioBase != null &&
+            //        usuario.UserID == usuarioBase.UserID &&
+            //        usuario.AccessKey == usuarioBase.AccessKey);
+            //}
+
+            if (credenciaisValidas)
+            {
+                ClaimsIdentity identity = new ClaimsIdentity(
+                    new GenericIdentity("usuario.Id", "Login"),
+                    new[] {
+                        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString("N")),
+                        new Claim(JwtRegisteredClaimNames.UniqueName, "usuario.Id")
+                    }
+                );
+
+                DateTime dataCriacao = DateTime.Now;
+                DateTime dataExpiracao = dataCriacao + TimeSpan.FromSeconds(tokenConfigurations.Seconds);
+
+                var handler = new JwtSecurityTokenHandler();
+                var securityToken = handler.CreateToken(new SecurityTokenDescriptor
+                {
+                    Issuer = tokenConfigurations.Issuer,
+                    Audience = tokenConfigurations.Audience,
+                    SigningCredentials = signingConfigurations.SigningCredentials,
+                    Subject = identity,
+                    NotBefore = dataCriacao,
+                    Expires = dataExpiracao
+                });
+                var token = handler.WriteToken(securityToken);
+
+                return new
+                {
+                    authenticated = true,
+                    created = dataCriacao.ToString("yyyy-MM-dd HH:mm:ss"),
+                    expiration = dataExpiracao.ToString("yyyy-MM-dd HH:mm:ss"),
+                    accessToken = token,
+                    message = "OK"
+                };
+            }
+            else
+            {
+                return new
+                {
+                    authenticated = false,
+                    message = "Falha ao autenticar"
+                };
+            }
+
+            return Response();
+        }
+
+    }
+}

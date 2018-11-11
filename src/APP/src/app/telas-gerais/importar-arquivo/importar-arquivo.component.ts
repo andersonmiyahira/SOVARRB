197842d3bc5a65b89f750d2aae6b1c59677bbc92
Anderson <andersonmiyahira@gmail.com>
2018-11-11 19:33:37 -0200

    ...

--- src/APP/src/app/telas-gerais/importar-arquivo/importar-arquivo.component.ts
+++ src/APP/src/app/telas-gerais/importar-arquivo/importar-arquivo.component.ts
@@ -7,25 +7,26 @@ import { BancoService } from 'app/telas-adm/banco/banco.service';
 import { Banco } from 'app/telas-adm/banco/models/banco';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { ETipoRegistro } from 'app/shared/enums/e-tipo-registro';
+import { ResultadoProcessadoResponse } from '../visualizar-arquivo/models/log-arquivo';
 
 @Component({
   selector: 'app-importar-arquivo',
   templateUrl: './importar-arquivo.component.html',
   styleUrls: ['./importar-arquivo.component.css'],
-  providers : [ImportarArquivo]
+  providers: [ImportarArquivo]
 })
 export class ImportarArquivoComponent implements OnInit {
-  
+
   public uploader: FileUploader = new FileUploader({ url: '' });
   public hasBaseDropZoneOver: boolean = false;
   public hasAnotherDropZoneOver: boolean = false;
   public form: FormGroup;
-  
+
   bancos: Array<Banco>;
   importar: any;
   model: ImportarArquivo;
 
-  mensagens: any; 
+  mensagens: ResultadoProcessadoResponse;
 
   arquivosValidados: boolean;
   exibirResultado: number;
@@ -35,13 +36,13 @@ export class ImportarArquivoComponent implements OnInit {
   @ViewChild("fileInput") fileInput;
 
   constructor(private importarArquivoService: ImportarArquivoService,
-              private modalService: NgbModal,
-              private bancoService: BancoService) {
-    this.importar = {}; 
+    private modalService: NgbModal,
+    private bancoService: BancoService) {
+    this.importar = {};
     this.arquivosValidados = false;
-    this.mensagens = {};
     this.exibirResultado = 0;
     this.model = new ImportarArquivo();
+    this.mensagens = new ResultadoProcessadoResponse();
   }
 
   ngOnInit() {
@@ -49,13 +50,13 @@ export class ImportarArquivoComponent implements OnInit {
     this.obterBancos();
     this.initFormGroup();
   }
-  
+
   saveUpload(sucesso): void {
 
     this.model.fileToUpload = [];
     this.uploader.queue.forEach(element => {
       this.model.fileToUpload.push(element.file.rawFile);
-    }); 
+    });
 
     this.importarArquivoService.upload(this.model)
       .subscribe(res => {
@@ -65,41 +66,22 @@ export class ImportarArquivoComponent implements OnInit {
         //this.fileInput.nativeElement.value = "";
         //this.arquivosValidados = true;
         this.modalService.open(sucesso, { size: 'sm' });
-    });
+      });
   }
 
   obterBancos() {
-    
+
     this.bancoService.obterBancos().subscribe(response => {
       this.bancos = response;
     });
-  } 
+  }
 
-  obterResultadoValidacao(filter: number){
-    this.importarArquivoService.obterResultadoValidacao(1).subscribe(response => {
-      if(filter > 0)
-        response = response.filter(_ => _["ehValido"]===(filter==1));
+  obterResultadoValidacao(filter: number, arquivoId: number) {
+    this.importarArquivoService.obterResultadoValidacao(arquivoId).subscribe(response => {
+      // if (filter > 0)
+      //   response = response.filter(_ => _["ehValido"] === (filter == 1));
 
-      this.mensagens.header = {};
-      this.mensagens.header.sucesso = [];
-      this.mensagens.header.erro = [];
-
-      this.mensagens.detalhe = {};
-      this.mensagens.detalhe.sucesso = [];
-      this.mensagens.detalhe.erro = [];
-
-      this.mensagens.trailer = {};
-      this.mensagens.trailer.sucesso = [];
-      this.mensagens.trailer.erro = [];
-
-      this.mensagens.header.sucesso = response.filter(x => x["tipo"] == 1 && x["ehValido"] == true);
-      this.mensagens.header.erro = response.filter(x => x["tipo"] == 1 && x["ehValido"] == false);
-
-      this.mensagens.detalhe.sucesso = response.filter(x => x["tipo"] == 2 && x["ehValido"] == true);
-      this.mensagens.detalhe.erro = response.filter(x => x["tipo"] == 2 && x["ehValido"] == false);
-
-      this.mensagens.trailer.sucesso = response.filter(x => x["tipo"] == 3 && x["ehValido"] == true);
-      this.mensagens.trailer.erro = response.filter(x => x["tipo"] == 3 && x["ehValido"] == false);
+      this.mensagens.resultado = response;
     });
   }
 
@@ -114,7 +96,7 @@ export class ImportarArquivoComponent implements OnInit {
       ]),
       tipoArquivo: new FormControl(this.model.tipoBoletoId, [
         Validators.required
-      ]) 
+      ])
     });
   }
 
